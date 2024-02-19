90da996adc09524ecea40942f7d3c27e3d2bd6be
PSK <tltmzh3@gmail.com>
2024-02-19 21:15:28 +0900

    [Commit] Tic 관련 함수 사용 - 시간 체크 용도

--- Optimized/Optimized/main.cpp
+++ Optimized/Optimized/main.cpp
@@ -6,6 +6,17 @@ using namespace std;
 
 void main() 
 {
-	cout << "Test Code" << endl;
+	int nCnt = 10;
+	DWORD avgTime = 0;
+	for (int j = 0; j < nCnt; j++) {
+		DWORD start = GetTickCount(); // 해당 함수는 1ms 해상도의 틱 카운트를 부호없는 32비트(4Byte) 정수로 변환함 
+		for (int i = 0; i < 5000000; i++); 
+		DWORD end = GetTickCount();
+		cout << "Startup Took " << end - start << " ms" << endl;
+		avgTime += (end - start);
+	}
+	avgTime /= nCnt;
+
+	cout << "Startup Avg Time : " << avgTime << " ms" << endl;// 5백만 기준 평균 3ms
 	system("pause");
 }
